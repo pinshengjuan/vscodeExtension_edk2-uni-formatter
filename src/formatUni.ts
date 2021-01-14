61cef0a29402bf142122b5d3a90b9e7984f491a0
Corey_Juan <corey_juan@asrock.com.tw>
2021-01-14 19:21:33 +0800

    change variable name for code readable

--- src/formatUni.ts
+++ src/formatUni.ts
@@ -7,21 +7,20 @@ function formatFile(filepath: string, maxStringLength: number): Promise<any> {
     /**
      * create local variable
      */
-    let spaceLengthWithoutString: number = maxStringLength + (HASH_STRING.length) + (EIGHT_SPACES.length);
-    let spaceLengthWithString: number;
-    let spaceWithoutString: string = '';
     let genSpaceCurrentNum: number = 0;
+    const langLineMaxSpaceAheadNum: number = maxStringLength + (HASH_STRING.length) + (EIGHT_SPACES.length);
+    let langLineMaxSpaceAhead: string = '';
+    let identifierLineMaxSpaceBehind: number;
     let fileString: string = '';
-    let tempString: string;
-    let stringName: string;
-    let stringNameLength: number;
-    let stringValue: string;
+    let identifierName: string;
+    let identifierNameLength: number;
+    let identifierValue: string;
 
     /**
      * 產生只有#language行的切齊點
      */
-    for(genSpaceCurrentNum=0 ; genSpaceCurrentNum<spaceLengthWithoutString ; genSpaceCurrentNum++) {
-      spaceWithoutString += ONE_SPACE;
+    for(genSpaceCurrentNum=0 ; genSpaceCurrentNum<langLineMaxSpaceAheadNum ; genSpaceCurrentNum++) {
+      langLineMaxSpaceAhead += ONE_SPACE;
     }
 
     /**
@@ -43,27 +42,27 @@ function formatFile(filepath: string, maxStringLength: number): Promise<any> {
         const patternString = new RegExp(/^#string/); //開頭是#string
         const patternLanguage = new RegExp((/^#language/)); //開頭是#language
         const patternComment = new RegExp(/^\/\//); //開頭是//
-        let spaceWithString: string = '';
+        let spacesBetweenIdentifierAndLang: string = '';
         if(line.match(patternString)) //這個判斷式處理開頭為#string的行
         {
-          stringName = line.split(' ')[1].trim().split('\t')[0]; //add .split('\t')[0] to avoid tab between stringName and #language
-          stringNameLength = stringName.length;//計算 #string後面接的字串的長度(e.g., #string ACPI_STR, 會計算ACPI_STR的長度，也就是8)
-          spaceLengthWithString = maxStringLength - stringNameLength; //Calculate spaces that needed between stringName and #language
-          stringValue = line.trim().split(HASH_LANGUAGE)[1]; //!!!Notice!!! this string got ONE space ahead
-          for(genSpaceCurrentNum = 0 ; genSpaceCurrentNum<spaceLengthWithString ; genSpaceCurrentNum++) {
-            spaceWithString += ONE_SPACE;
-          }// 此迴圈將當前stringName後面的空白補得和該檔案內stringName長度最長者一樣
-          spaceWithString = spaceWithString + EIGHT_SPACES; //將stringName 和 #language中間加8個空白
-          fileString = fileString + HASH_STRING + stringName + spaceWithString + HASH_LANGUAGE + stringValue + '\n';
+          identifierName = line.split(' ')[1].trim().split('\t')[0]; //add .split('\t')[0] to avoid tab between identifierName and #language
+          identifierNameLength = identifierName.length;//計算 #string後面接的字串的長度(e.g., #string ACPI_STR, 會計算ACPI_STR的長度，也就是8)
+          identifierLineMaxSpaceBehind = maxStringLength - identifierNameLength; //Calculate spaces that needed between identifierName and #language
+          for(genSpaceCurrentNum=0 ; genSpaceCurrentNum<identifierLineMaxSpaceBehind ; genSpaceCurrentNum++) {
+            spacesBetweenIdentifierAndLang += ONE_SPACE;
+          }// 此迴圈將當前identifierName後面的空白補得和該檔案內identifierName長度最長者一樣
+          
+          spacesBetweenIdentifierAndLang = spacesBetweenIdentifierAndLang + EIGHT_SPACES; //將identifierName 和 #language中間加8個空白
+          identifierValue = line.trim().split(HASH_LANGUAGE)[1]; //!!!Notice!!! this string got ONE space ahead
+          fileString = fileString + HASH_STRING + identifierName + spacesBetweenIdentifierAndLang + HASH_LANGUAGE + identifierValue + '\n';
+        }
+        else if(line.trim().match(patternLanguage)) //這段處理前面是一堆空白接#language的行
+        {
+          fileString = fileString + langLineMaxSpaceAhead + line.trim() + '\n';
         }
         else if(line.trim() === '') //將只有Tab或/和space的行換成一換行符號
         {
-          fileString = fileString + '\n';
-        }
-        else if(line.trim().match(patternLanguage)) //處理開頭是一堆空白接#language的行
-        {
-          tempString = line.trim();
-          fileString = fileString + spaceWithoutString + tempString + '\n';
+          fileString += '\n';
         }
         else
         {
